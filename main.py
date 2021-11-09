2ff7360d8f3f3da52d91cdfe227c00d08613b0c9
Ricardo Diaz <ing.rdiaz@gmail.com>
2021-11-09 09:08:17 -0500

    validation_query commit

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 #Python
 from typing import Optional
+from fastapi.param_functions import Query
 
 #Pydantic
 from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body #clase que permite decir que un parametro de una clase es de tipo Body
+from fastapi import Body, Query #clase que permite decir que un parametro de una clase es de tipo Body y Query
 
 
 app = FastAPI()
 
-#models
+# Models
 
 class Person(BaseModel):
     first_name: str
@@ -31,3 +32,14 @@ def home():
 def create_person(person: Person = Body(...)):
     #Body(...) indica que el parametro es obligatorio
     return person
+
+# Validations : Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    #parametro name opcional y se valida que minimo tenga un caracter y max 50 y como defailt None
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    #parametro age obligatorio y se valida que minimo tenga un caracter y max 50 y como defailt None
+    age: str = Query(...)
+):
+    return {name: age}
